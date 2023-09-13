88fc347c82f6add9b710224c8f88a254ace82c11
shubhangi-ambit <129859496+shubhangi-ambit@users.noreply.github.com>
2023-09-13 16:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,23 +66,26 @@ def main():
         image_path = "company_metrics_graph.png"
         fig.savefig(image_path, bbox_inches='tight', dpi=100)
 
-        # Create a replicate Excel workbook and add the data and graph to it
+        # Create a replicate Excel workbook with filtered data and the graph
         output = io.BytesIO()
         with xlsxwriter.Workbook(output, {'in_memory': True}) as workbook:
-            # Add a worksheet and write the data
+            # Add a worksheet and write the filtered data
             worksheet = workbook.add_worksheet("Company Data")
-            for i, col in enumerate(df.columns):
+            for i, col in enumerate(company_data.columns):
                 worksheet.write(0, i, col)
-            for i, value in enumerate(df.values):
+            for i, value in enumerate(company_data.values):
                 for j, val in enumerate(value):
                     worksheet.write(i + 1, j, val)
 
-            # Insert the saved image at the end of the Excel file
-            worksheet.insert_image('A1', image_path, {'x_offset': 0, 'y_offset': 0, 'x_scale': 0.5, 'y_scale': 0.5})
+            # Calculate the number of rows and columns in the data
+            num_rows, num_cols = company_data.shape
+
+            # Insert the saved image to the right of the data
+            worksheet.insert_image(num_rows + 2, 0, image_path, {'x_offset': 0, 'y_offset': 0, 'x_scale': 0.5, 'y_scale': 0.5})
 
         # Allow the user to download the Excel file
-        st.markdown("### Download Excel with Data and Graph")
-        st.download_button(label="Download Excel", data=output.getvalue(), file_name="company_metrics.xlsx", key="download")
+        st.markdown("### Download Excel with Filtered Data and Graph")
+        st.download_button(label="Download Excel", data=output.getvalue(), file_name="company_metrics_filtered.xlsx", key="download")
 
 if __name__ == "__main__":
     main()
